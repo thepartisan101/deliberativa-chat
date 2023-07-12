fb38106b8980ebe065f29f23dd2f5d2d42070623
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2023-07-12 15:12:46 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Deliberativa AI Chatbot',
+    template: `%s - Deliberativa AI Chatbot`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
   themeColor: [
