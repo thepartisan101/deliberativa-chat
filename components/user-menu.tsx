d58d37f179cec9407e38b097d0084aa788c5f29c
thepartisan101 <9511307+thepartisan101@users.noreply.github.com>
2023-07-12 13:13:12 +0000

    comment irreleavant button

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -49,7 +49,7 @@ export function UserMenu({ user }: UserMenuProps) {
             <div className="text-xs text-zinc-500">{user?.email}</div>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
+          {/* <DropdownMenuItem asChild>
             <a
               href="https://vercel.com"
               target="_blank"
@@ -59,7 +59,7 @@ export function UserMenu({ user }: UserMenuProps) {
               Vercel Homepage
               <IconExternalLink className="w-3 h-3 ml-auto" />
             </a>
-          </DropdownMenuItem>
+          </DropdownMenuItem> */}
           <DropdownMenuItem
             onClick={() =>
               signOut({
